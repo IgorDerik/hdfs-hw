836486d42dd02fe370d8e35fd962cd79441636b4
Igor Dereviankin <Igor_Dereviankin@epam.com>
2018-12-17 01:06:28 +0200

    System.setProperty hadoop.home.dir to C.hadoop disabled

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        System.setProperty("hadoop.home.dir","C:\\hadoop" );
+//        System.setProperty("hadoop.home.dir","C:\\hadoop" );
 
 //        Utils.convertCsvToParquet(args[0],args[1]);
 //        Utils.convertCsvToParquet("hdfs://localhost:8020/user/admin/matches.csv","hdfs://localhost:8020/user/admin/par111");
