ea4ff855376881abe2e229283c2ddad2cb7fdd07
Igor Dereviankin <Igor_Dereviankin@epam.com>
2018-12-27 16:14:03 +0200

    changes added

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,19 +1,10 @@
 package com.app;
 
-import java.util.List;
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
 
 public class Main {
 
     public static void main(String[] args) {
-/*
-        ParquetUtils.testCSVReader("src/main/resources/test.csv",100);
-        Schema schema = ParquetUtils.parseSchema("src/main/resources/t50.avsc");
-        List<GenericData.Record> recordList = ParquetUtils.getRecords(schema,"src/main/resources/t50.csv");
-        ParquetUtils.writeToParquet(recordList,schema,"src/main/resources/t50.parquet");
-        ParquetUtils.readParquetFile("src/main/resources/t50.parquet",100);
-*/
 
         Schema schema = ParquetUtils.parseSchema(args[0]);
 
@@ -21,6 +12,7 @@ public class Main {
 
         ParquetUtils.readParquetFile(args[2],Integer.parseInt(args[3]));
 
+//        ParquetUtils.readParquetFile("src/test/resources/fileForRead.parquet",5);
     }
 
 }
